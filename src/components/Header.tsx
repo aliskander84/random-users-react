55302954c921350019041665ba1a2ce6c770ac4e
alexchegash <aliskander84@gmail.com>
2020-04-11 23:21:30 +0300

    added material-ui, removed bootstrap, done raw Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+// import {NavLink} from 'react-router-dom'
+import {AppBar, Button, IconButton, Toolbar, Typography} from '@material-ui/core'
+// import {SupervisorAccount as UsersIcon} from '@material-ui/icons'
 
 export const Header: React.FC = () => {
+  const Title = <>
+    <Typography variant="h5">
+      Random users
+    </Typography>
+  </>
+
   return <>
-    <NavLink to={'/'}>Home</NavLink> |
-    <NavLink to={'/user'}>User</NavLink>
+    <AppBar position="static">
+      <Toolbar>
+        {Title}
+      </Toolbar>
+    </AppBar>
   </>
 }
